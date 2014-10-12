c21ef64cb4dfd48388494bf58a39e89ade663bbe
Silvia <silvia@milkyway.(none)>
2014-10-12 07:28:06 +0200

    insert the entries for the user-cases

---
+++ SQL_scripts/insertsDB.sql
@@ -0,0 +1,13 @@
+INSERT INTO `RESOURCES` (`Title`, `Link`, `AuthorId`, `Fingerprint`) VALUES
+('Neural networks', 'http://pages.cs.wisc.edu/~bolo/shipyard/neural/local.html', 21, '242,88,87,89,91,93,95,90'),
+('Neural networks', 'https://infosimples.com/en/articles/overview-neural-networks', 25, '132,133,140,1,78,142,146'),
+('Bological neural networks', 'http://www.teco.edu/~albrecht/neuro/html/node7.html', 20, '246,247,248,244,243,241,201,242,89'),
+('Biological neural networks', 'http://wwwold.ece.utep.edu/research/webfuzzy/docs/kk-thesis/kk-thesis-html/node10.html', 24, '246,247,248,244,243,241,201,242,89'),
+('Multilayer Perceptron Neural Network Model', 'http://www.dtreg.com/mlfn.htm', 23, '1,78,79,88,91,83,82,85,81,84'),
+('Neural networks-neuron', 'http://cs.stanford.edu/people/eroberts/courses/soco/projects/neural-networks/Neuron/index.html', 22, '1,78,79,88,91,80'),
+('Central dogma of genetics', 'http://www.ndsu.edu/pubweb/~mcclean/plsc431/cloning/clone1.htm', 21, '242,88,87,89,91,93,95,90'),
+('Central dogma', 'http://www.ndsu.edu/pubweb/~mcclean/plsc731/dna/dna1.htm', 20, '246,247,248,244,243,241,201,242,89'),
+('Central dogma of genetics', 'http://www.exploringnature.org/db/detail.php?dbID=106&detID=2454', 25, '132,133,140,1,78,142,146'),
+('The central dogma of molecular biology', 'http://www.accessexcellence.org/RC/VL/GG/central.php', 24, '246,247,248,244,243,241,201,242,89'),
+('Central dogma of biology', 'http://www.dnalc.org/resources/3d/central-dogma.html', 23, '1,78,79,88,91,83,82,85,81,84'),
+('Central dogma of genetics - Classic view', 'http://www.ncbi.nlm.nih.gov/Class/MLACourse/Modules/MolBioReview/central_dogma.html', 22, '1,78,79,88,91,80');
